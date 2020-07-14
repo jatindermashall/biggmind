b0357c9fccd76ef19013e8b8a063fe3eb91d4f03
jatindermashall <jatindermashall@gmail.com>
2020-07-14 10:52:43 +0530

    books area

--- components/Profile_test.vue
+++ components/Profile_test.vue
@@ -137,12 +137,15 @@
                 <h1 class="bg-transparent">Books recomendation</h1>
                 <hr />
                 <p class="bg-transparent">
-                  Dummy content goes here. These lists are meant to identify
-                  articles which deserve editor attention because they are the
-                  most important for an encyclopedia to have, as determined by
-                  the community of participating editors. They may also be of
-                  interest to readers as an alternative to lists of overview
-                  articles.
+
+                  <div class="parent">
+  <div class="child" v-for="(book, index) in profile.books" :key="index">
+    <img :src="endPoint + book.image[0].url" width="100px"/><br/>
+    
+    {{index + 1}}. {{book.title}}</div>
+  
+</div>
+                  
                 </p>
               </div>
             </div>
