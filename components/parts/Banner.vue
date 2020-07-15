738f075e1523660910fc595cb5fc58c998d41c7e
Nidhishar <nidhi191993@gmail.com>
2020-07-15 15:01:21 +0530

    search bar implementation

--- components/parts/Banner.vue
+++ components/parts/Banner.vue
@@ -7,24 +7,27 @@
           <input
             class="form-control searchbar"
             type="search"
+            v-model="search"
             name
             id
             placeholder="Search stories, profiles, news etc.."
           />
           <div class="btn-group searchdropdown align-items-center">
             <button
+              id="dropdownToSelect"
               type="button"
               class="btn btn-light dropdown-toggle"
               data-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
-            >Select</button>
-            <div class="dropdown-menu">
-              <button class="dropdown-item" type="button">Profiles</button>
-              <button class="dropdown-item" type="button">Stories</button>
-              <button class="dropdown-item" type="button">News</button>
+              @click="showDropdown = !showDropdown"
+            >{{selected ? selected : 'Select'}}</button>
+            <div class="dropdown-menu" :class="{'show': showDropdown}" aria-labelledby="dropdownToSelect">
+              <button class="dropdown-item" value="profiles" @click="selected = 'Profiles', showDropdown = false">Profiles</button>
+              <button class="dropdown-item" value="stories" @click="selected = 'Stories', showDropdown = false">Stories</button>
+              <button class="dropdown-item" value="news" @click="selected = 'News', showDropdown = false">News</button>
             </div>
-            <button type="submit" class="btn ml-1 searchbtn">Search</button>
+            <button type="submit" class="btn ml-1 searchbtn" @click="searchFilter">Search</button>
           </div>
         </form>
       </div>
@@ -33,7 +36,24 @@
   <!--Search bar ends here-->
 </template>
 <script>
-export default {};
+import {mapActions} from 'vuex'
+export default {
+  data(){
+    return{
+      selected: null,
+      search: '',
+      showDropdown: false
+    }
+  },
+  methods: {
+    ...mapActions('profiles', ['setSearch']),
+    async searchFilter() {
+      if (this.selected) {
+        await this.setSearch({text: this.search, type: this.selected})
+      }
+    }
+  }
+};
 </script>
 
 <style>
--- store/profiles.js
+++ store/profiles.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const state = {
   profiles: [],
-  selectedProfile: {}
+  selectedProfile: {},
+  filtersearch: {}
 };
 
 const getters = {
@@ -24,12 +25,25 @@ const actions = {
     console.log(urlp);
     const response = await axios.get(`http://161.35.7.108/profiles/${urlp[0]}`);
     commit("setSelectedProfile", response.data);
+  },
+  setSearch ({ state,commit, dispatch }, payload) {
+    if (payload.type.toLowerCase() === 'profiles') {
+      if (payload.text !== '') {
+        let result =  state.profiles.filter(item => {
+          return item['full_name'].toLowerCase().includes(payload.text.toLowerCase()) })
+        commit("setProfiles", result);
+      } else {
+        dispatch('fetchProfiles')
+      }
+    }
+    commit('setSearch', payload)
   }
 };
 
 const mutations = {
   setProfiles: (state, profiles) => (state.profiles = profiles),
-  setSelectedProfile: (state, profile) => (state.selectedProfile = profile)
+  setSelectedProfile: (state, profile) => (state.selectedProfile = profile),
+  setSearch: (state,payload) => (state.filtersearch = payload)
 };
 
 export default {
