57caf56f7a09de722d1da58bd43067e558e5b033
jatinder <jatindermashall@gmail.com>
2020-07-29 11:42:03 +0530

    change api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   env: {
-    apiUrl: process.env.API_URL || "http://161.35.7.108",
-    imageUrl: process.env.API_IMAGE_URL || "http://161.35.7.108"
+    apiUrl: process.env.API_URL || "https://mlisting.com",
+    imageUrl: process.env.API_IMAGE_URL || "https://mlisting.com"
   },
   /*
    ** Nuxt rendering mode
