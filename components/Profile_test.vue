81fb386bb880c3a3086a6dec8cc9a5acb4caa0c0
jatinder <jatindermashall@gmail.com>
2020-08-01 19:35:52 +0530

    css changes

--- components/Profile_test.vue
+++ components/Profile_test.vue
@@ -8,96 +8,35 @@
             class="col-md-3 col-sm-12 littleheaddetail text-center align-self-center column justify-content-lg-center align-items-lg-center"
           >
             
-        
-              <v-card class="d-inline-block mx-auto" v-if="profile.profile_image">
-    <v-container>
-      <v-row justify="space-between">
-        <v-col cols="auto">
-          <v-img :src="endPoint + profile.profile_image[0].url"
-            height="200"
-            width="200"
-            
-          ></v-img>
-{{profile.name}}
-
-        </v-col>
-
-        <v-col
-          cols="auto"
-          class="text-center pl-0"
-        >
-          <v-row
-            class="flex-column ma-0 fill-height"
-            justify="center"
-          >
-            <v-col class="px-0">
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-            </v-col>
-
-            <v-col class="px-0">
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-            </v-col>
-
-            <v-col class="px-0">
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+        <div class="d-inline-block"  v-if="profile.profile_image">
+              <span class="round-profile"><img
+                class="profileimage img-fluid" :src="endPoint + profile.profile_image[0].url"/></span>
+              <h2 class="text-center profileheader">{{profile.name}}</h2>
+              <button class="social-profile"><i aria-hidden="true" class="fa fa-heart"></i></button>
+              <button class="social-profile"><i aria-hidden="true" class="fa fa-bookmark"></i></button>
+              <button class="social-profile"><i aria-hidden="true" class="fa fa-share-alt"></i></button>
+            </div>
+              
             
           </div>
           <div class="col-md-9 col-sm-12 bg-transparent">
-            <div class="card herocard text-left shadow" style="max-width: 800px;">
+            <div
+              class="card herocard text-left shadow">
               <div class="card-body align-items-center d-flex">
-                <div class="col-md-6 col-sm-12">
-                  <h4 style="margin-bottom: 0px; color: #7c7c7c !important;">
-                    Content for the header goes here&nbsp;
-                    <span>like networth etc etc</span>
-                  </h4>
-                  <h6 style="margin-top: 10px; color: #7c7c7c !important;">
-                    Content for the header goes here&nbsp;
-                    <span>like networth etc etc</span>
-                  </h6>
-                </div>
-
-                <div
-                  class="col-md-6 col-sm-12 d-flex flex-row rightprofileheaderbox align-content-center justify-content-lg-center align-items-lg-center"
-                  style="margin-top: 5px;"
-                >
-                  <div>
-                    <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-pinterest fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                    <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                    <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                    <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </div>
+                <div class="col-md-12 col-sm-12">
+                <table cellpadding="0" cellspacing="0" class="userDes">
+                 <tr><th><i class="fa fa-user" aria-hidden="true"></i> Born:</th> <td>12 January 1964 (age 56 years)</td></tr>
+                 <tr><th><i class="fa fa-sort-amount-desc" aria-hidden="true"></i> Height:</th> <td> 1.71 m</td></tr>
+                 <tr><th><i class="fa fa-usd" aria-hidden="true"></i> Net worth:</th> <td> 18,040 crores USD (2020)</td></tr>
+                 <tr><th><i class="fa fa-users" aria-hidden="true"></i> Children:</th> <td> 4</td></tr>
+                 <tr><th><i class="fa fa-graduation-cap" aria-hidden="true"></i> Education:</th> <td> Princeton University (1982–1986), River Oaks Elementary School, Miami Palmetto Senior High School</td></tr>
+                 <tr><th><i class="fa fa-briefcase" aria-hidden="true"></i> Source of Wealth:</th> <td> Amazon, Self Made</td></tr>
+                 <tr><th><i class="fa fa-home" aria-hidden="true"></i> Residence:</th> <td> Seattle, Washington</td></tr>
+                 <tr><th><i class="fa fa-globe" aria-hidden="true"></i> Citizenship:</th> <td> United States</td></tr>
+                 <tr><th><i class="fa fa-share-alt" aria-hidden="true"></i> Social:</th> <td class="socialIco"> <a href="#"><i class="fa fa-facebook-square"></i></a> <a href="#"><i class="fa fa-twitter-square"></i></a> <a href="#"><i class="fa fa-linkedin-square"></i></a> <a href="#"><i class="fa fa-google-plus-square"></i></a></td></tr>
+                  </table>
+                  
+                  
                 </div>
               </div>
             </div>
@@ -113,34 +52,32 @@
         <!-- tabs -->
         <div class="tabbable d-flex bg-transparent container">
           <div
-            class="text-center boxedcontainer tabhandler col-md-3 col-sm-12 flex-column d-flex sidebarHeight"
+            class="text-center boxedcontainer tabhandler col-md-3 col-sm-12 flex-column d-flex"
           >
-            <ul class="nav tabpillscustomul bg-transparent">
-              <span class="tabpillscustom">
-                <li class="active">
-                  <a href="#home" style="background-color: none;" @click="selectedTab = 'home'" data-toggle="tab">Overview</a>
+            <ul  class="nav tabpillscustomul">
+              
+                <li class="active tabpillscustom">
+                  <a href="#home" data-toggle="tab"  class="active" @click="selectedTab = 'home'" ><i aria-hidden="true" class="fa fa-heart"></i> Overview</a>
+                  
                 </li>
-              </span>
-              <span class="tabpillscustom">
-                <li>
-                  <a href="#Quotes" data-toggle="tab" @click="selectedTab = 'quotes'">Quotes</a>
+              
+                <li class="tabpillscustom">
+                  <a href="#Quotes" data-toggle="tab" @click="selectedTab = 'quotes'"><i aria-hidden="true" class="fa fa-quote-left"></i> Quotes</a>
+                  
                 </li>
-              </span>
-              <span class="tabpillscustom">
-                <li>
+              
+                <li class="tabpillscustom">
                   <a href="#Books" data-toggle="tab" @click="selectedTab = 'books'">Books</a>
                 </li>
-              </span>
-              <span class="tabpillscustom">
-                <li>
+              
+                <li class="tabpillscustom">
                   <a href="#Stories" data-toggle="tab" @click="selectedTab = 'stories'">Stories</a>
                 </li>
-              </span>
-              <span class="tabpillscustom">
-                <li>
+              
+                <li class="tabpillscustom">
                   <a href="#News" data-toggle="tab" @click="selectedTab = 'news'">News</a>
                 </li>
-              </span>
+              
             </ul>
           </div>
 
@@ -157,7 +94,8 @@
                 <h1 class="bg-transparent">Quotes</h1>
                 <hr />
                 <p class="bg-transparent">
-                  <h5 v-for="(quote, index) in profile.quotes" :key="index">{{index + 1}}. {{quote.content}}</h5>
+                 <ul class="listing"> <li v-for="(quote, index) in profile.quotes" :key="index">{{index + 1}}. {{quote.content}}</li>
+                 </ul>
                 </p>
               </div>
             </div>
