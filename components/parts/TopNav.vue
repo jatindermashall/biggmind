2b1f628d394b84ac1a02e8978781429a2f37fdad
jatindermashall <jatindermashall@gmail.com>
2020-08-25 14:56:51 +0530

    environment

--- components/parts/TopNav.vue
+++ components/parts/TopNav.vue
@@ -5,19 +5,12 @@
     style="background-color: rgb(34, 34, 34);"
   >
     <div class="container bg-transparent">
-      <a class="navbar-brand" href="#">Company Logo</a>
-      <button
-        data-toggle="collapse"
-        class="navbar-toggler bg-transparent"
-        data-target="#navcol-1"
-      >
+      <a class="navbar-brand" href="#">Biggmind</a>
+      <button data-toggle="collapse" class="navbar-toggler bg-transparent" data-target="#navcol-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div
-        class="collapse bg-transparent navbar-collapse justify-content-end"
-        id="navcol-1"
-      >
+      <div class="collapse bg-transparent navbar-collapse justify-content-end" id="navcol-1">
         <ul class="nav navbar-nav">
           <li class="nav-item" role="presentation">
             <a class="nav-link active" href="#">Home</a>
@@ -37,8 +30,7 @@
             "
           role="button"
           href="#"
-          >Logout</a
-        >
+        >Login</a>
       </div>
     </div>
   </nav>
